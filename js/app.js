375c51ffd54773df31994213b003cc7dc690a11d
Bruno Bertol <bbertol2312@outlook.com>
2025-04-09 16:00:39 -0300

    Implementando confirmação da ação de devolver ou de alugar

--- js/app.js
+++ js/app.js
@@ -3,14 +3,20 @@ function alterarStatus(id) {
     let imagem = gameSelecionado.querySelector('.dashboard__item__img');
     let button = gameSelecionado.querySelector('.dashboard__item__button');
 
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        button.classList.remove('dashboard__item__button--return');
-        button.innerHTML = 'Alugar';
-    } else {
-        imagem.classList.add('dashboard__item__img--rented')
-        button.innerHTML = 'Devolver';
-        button.classList.add('dashboard__item__button--return')
-    }
+    let wordsConfirmation = imagem.classList.contains('dashboard__item__img--rented') ? 'Devolver' : 'Alugar';
+    let confirmation = confirm(`Tem certeza que deseja ${wordsConfirmation} este item?`)
 
+    if (confirmation == true) {
+
+        if (imagem.classList.contains('dashboard__item__img--rented')) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            button.classList.remove('dashboard__item__button--return');
+            button.innerHTML = 'Alugar';
+        } else {
+            imagem.classList.add('dashboard__item__img--rented')
+            button.innerHTML = 'Devolver';
+            button.classList.add('dashboard__item__button--return')
+        }
+    }
+        
 }
